898645d6da292b8a2ed4632a6c26f1ba435417c8
gilbert <parksechae@gmail.com>
2019-02-19 17:38:16 +0900

    add api for servers

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,40 +8,51 @@ import * as functions from 'firebase-functions';
 // });
 
 export const users = functions.https.onRequest((req, res) => {
-        const init_users = [
-            { "name": "홍길동", "email": "blabla@company.com" },
-            { "name": "김길동", "email": "blabla@company.com" },
-            { "name": "박길동", "email": "blabla@company.com" },
-            { "name": "이길동", "email": "blabla@company.com" },
-            { "name": "최길동", "email": "blabla@company.com" },
-            { "name": "박세채", "email": "blabla@company.com" },
-            { "name": "Liam", "email": "blabla@company.com" },
-            { "name": "Noah", "email": "blabla@company.com" },
-            { "name": "Logan", "email": "blabla@company.com" },
-            { "name": "Lucas", "email": "blabla@company.com" },
-            { "name": "Mason", "email": "blabla@company.com" },
-            { "name": "Oliver", "email": "blabla@company.com" },
-            { "name": "Elijah", "email": "blabla@company.com" },
-            { "name": "Aiden", "email": "blabla@company.com" },
-            { "name": "James", "email": "blabla@company.com" }
+    const init_users = [
+        { "name": "홍길동", "email": "blabla@company.com" },
+        { "name": "김길동", "email": "blabla@company.com" },
+        { "name": "박길동", "email": "blabla@company.com" },
+        { "name": "이길동", "email": "blabla@company.com" },
+        { "name": "최길동", "email": "blabla@company.com" },
+        { "name": "박세채", "email": "blabla@company.com" },
+        { "name": "Liam", "email": "blabla@company.com" },
+        { "name": "Noah", "email": "blabla@company.com" },
+        { "name": "Logan", "email": "blabla@company.com" },
+        { "name": "Lucas", "email": "blabla@company.com" },
+        { "name": "Mason", "email": "blabla@company.com" },
+        { "name": "Oliver", "email": "blabla@company.com" },
+        { "name": "Elijah", "email": "blabla@company.com" },
+        { "name": "Aiden", "email": "blabla@company.com" },
+        { "name": "James", "email": "blabla@company.com" }
 
-        ];
+    ];
 
-        const match_users = init_users.filter(user => {
+    let match_users = [];
+
+    if ("all" === req.query.text.toString().toLowerCase()) {
+        match_users = init_users;
+    } else {
+        match_users = init_users.filter(user => {
             return user["name"].indexOf(req.query.text) > -1
         });
+    }
 
-        const resData = {
-            // "request method": req.method,
-            // "request query": req.query,
-            // "request body": req.body,
-            "startAt": 0,
-            "maxResults": 10,
-            "total": match_users.length,
-            "users": match_users
-        };
+    let resText = "";
 
-        res.contentType('application/json');
-        res.send(resData);
-    })
-;
+    for (const user of match_users) {
+        resText += "Name : " + user["name"] + ", " + "Email : " + user["email"] + "\n"
+    }
+
+    if (resText.trim().length < 1) {
+        resText += "Not Found (Search Keyword : " + req.query.text + ")\n";
+        resText += "Type 'all' for all list"
+    }
+
+    const resData = {
+        "text": resText
+    };
+
+    res.contentType('application/json');
+    res.send(resData);
+});
+
